cc14e0e2bc476dac1e77d16a6089567a9230733f
Moppit <michelletranusa@gmail.com>
2020-05-04 13:24:10 -0600

    Finished 3

--- project_euler/3.go
+++ project_euler/3.go
@@ -6,8 +6,42 @@ What is the largest prime factor of the number 600851475143 ?
 package main
 
 import "fmt"
+import "math"
+
+// Simple method: try heuristic method later on
+// 6k +/- 1 omptimization
+func isPrime(num int) bool {
+  // Check cases that often happen for ease of computation
+  // Deals with 1-3 case
+  if num <= 3 {
+    return num > 1
+  } else if num%2 == 0 || num%3 == 0 || num%5 == 0 {
+    return false // Handles multiples of 2,3,5
+  }
+
+  // Case that deals with more difficult values to evaluate
+  i := 5
+  for i*i <= num {
+    // Check if divisible by i or i+2
+    if num%i == 0 || num%(i+2) == 0 {
+      return false
+    }
+    i += 6
+  }
+  return true
+}
 
 func main() {
-  // Well I know it isn't even, so I don't have to go all the way to that large number
-  // Can go to like half of it... also need to check if the value is prime
+  // Primality testing typically goes from 2 to sqrt(n) -- from number theory,
+  // composite numbers have factors less than or equal to its root
+  toFactor := 600851475143
+  start := int(math.Sqrt(float64(toFactor)))
+  for i := start; i >= 2; i-- {
+    if isPrime(i) && toFactor%i == 0 {
+      fmt.Println(i)
+      break
+    }
+  }
 }
+
+// Result = 6857
